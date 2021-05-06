2745d0bf4e7be4a4b7fa1f32a922852fa5804cc5
jason <jasoa125@gmail.com>
2021-05-06 15:01:07 +0800

    FIX

--- src/utils/dgraph-client.js
+++ src/utils/dgraph-client.js
@@ -1,18 +1,18 @@
-const dgraph = require('dgraph-js-http');
+const dgraph = require('dgraph-js-http')
 
 // Create a client stub.
 function newClientStub() {
-  return new dgraph.DgraphClientStub('http://localhost:8080');
+  return new dgraph.DgraphClientStub('http://localhost:8080')
 }
 
 // Create a client.
 function newClient(clientStub) {
-  return new dgraph.DgraphClient(clientStub);
+  return new dgraph.DgraphClient(clientStub)
 }
 
 // Drop All - discard all data and start from a clean slate.
 async function dropAll(dgraphClient) {
-  await dgraphClient.alter({ dropAll: true });
+  await dgraphClient.alter({ dropAll: true })
 }
 
 // Set schema.
@@ -23,15 +23,15 @@ async function setSchema(dgraphClient) {
     check: string .
     next: uid .
     process_edge_name: string .
-  `;
-  await dgraphClient.alter({ schema: schema });
-  console.log('Set schema success!');
+  `
+  await dgraphClient.alter({ schema: schema })
+  console.log('Set schema success!')
 }
 
 // Create data using JSON.
 async function createData(dgraphClient) {
   // Create a new transaction.
-  const txn = dgraphClient.newTxn();
+  const txn = dgraphClient.newTxn()
   try {
     // Create data.
     const p = [
@@ -43,7 +43,7 @@ async function createData(dgraphClient) {
           {
             'dgraph.type': 'ProcessEdge',
             uid: '_:rootToFirst',
-            process_edge_name: 'whatever',
+            process_edge_name: '(無論如何)',
             check: 'whatever',
             next: {
               'dgraph.type': 'Process',
@@ -53,7 +53,7 @@ async function createData(dgraphClient) {
                 {
                   'dgraph.type': 'ProcessEdge',
                   uid: '_:firstToSecond',
-                  process_edge_name: 'whatever',
+                  process_edge_name: '(無論如何)',
                   check: 'whatever',
                   next: {
                     'dgraph.type': 'Process',
@@ -62,7 +62,7 @@ async function createData(dgraphClient) {
                     processEdges: [
                       {
                         'dgraph.type': 'ProcessEdge',
-                        process_edge_name: 'over_than_3_days',
+                        process_edge_name: '(病假且大於3天)',
                         check: "{leaveDays} > 3 && {leaveType} == '病假'",
                         next: {
                           'dgraph.type': 'Process',
@@ -86,7 +86,7 @@ async function createData(dgraphClient) {
           {
             'dgraph.type': 'ProcessEdge',
             uid: '_:rootToFirst',
-            process_edge_name: 'whatever',
+            process_edge_name: '(無論如何)',
             check: 'whatever',
             next: {
               'dgraph.type': 'Process',
@@ -96,7 +96,7 @@ async function createData(dgraphClient) {
                 {
                   'dgraph.type': 'ProcessEdge',
                   uid: '_:firstToSecond',
-                  process_edge_name: 'whatever',
+                  process_edge_name: '(無論如何)',
                   check: 'whatever',
                   next: {
                     'dgraph.type': 'Process',
@@ -105,7 +105,7 @@ async function createData(dgraphClient) {
                     processEdges: [
                       {
                         'dgraph.type': 'ProcessEdge',
-                        process_edge_name: 'over_than_3_days',
+                        process_edge_name: '(事假且大於3天)',
                         check: "{leaveDays} > 3 && {leaveType} == '事假'",
                         next: {
                           'dgraph.type': 'Process',
@@ -129,7 +129,7 @@ async function createData(dgraphClient) {
           {
             'dgraph.type': 'ProcessEdge',
             uid: '_:rootToFirst',
-            process_edge_name: 'whatever',
+            process_edge_name: '(無論如何)',
             check: 'whatever',
             next: {
               'dgraph.type': 'Process',
@@ -139,7 +139,7 @@ async function createData(dgraphClient) {
                 {
                   'dgraph.type': 'ProcessEdge',
                   uid: '_:firstToSecond',
-                  process_edge_name: 'whatever',
+                  process_edge_name: '(無論如何)',
                   check: 'whatever',
                   next: {
                     'dgraph.type': 'Process',
@@ -148,12 +148,24 @@ async function createData(dgraphClient) {
                     processEdges: [
                       {
                         'dgraph.type': 'ProcessEdge',
-                        process_edge_name: 'over_than_5_days',
+                        process_edge_name: '(婚假且大於5天)',
                         check: "{leaveDays} > 5 && {leaveType} == '婚假'",
                         next: {
                           'dgraph.type': 'Process',
-                          uid: '_:generalManagerSign',
-                          process_name: '總經理簽核',
+                          uid: '_:presonalManagerSign',
+                          process_name: '人事主管簽核',
+                          processEdges: [
+                            {
+                              'dgraph.type': 'ProcessEdge',
+                              process_edge_name: '(無論如何)',
+                              check: 'whatever',
+                              next: {
+                                'dgraph.type': 'Process',
+                                uid: '_:generalManagerSign',
+                                process_name: '總經理簽核',
+                              },
+                            },
+                          ],
                         },
                       },
                     ],
@@ -164,13 +176,13 @@ async function createData(dgraphClient) {
           },
         ],
       },
-    ];
+    ]
 
     // Run mutation.
-    await txn.mutate({ setJson: p });
+    await txn.mutate({ setJson: p })
 
     // Commit transaction.
-    await txn.commit();
+    await txn.commit()
 
     // Get uid of the outermost object (person named "Alice").
     // Assigned#getUidsMap() returns a map from blank node names to uids.
@@ -180,21 +192,21 @@ async function createData(dgraphClient) {
     //   `Created person named "Alice" with uid = ${assigned.data.uids['blank-0']}\n`
     // );
 
-    console.log('All created nodes (map from blank node names to uids):');
+    console.log('All created nodes (map from blank node names to uids):')
     // Object.keys(assigned.data.uids).forEach((key) =>
     //   console.log(`${key} => ${assigned.data.uids[key]}`)
     // );
   } finally {
     // Clean up. Calling this after txn.commit() is a no-op
     // and hence safe.
-    await txn.discard();
+    await txn.discard()
   }
 }
 
 // Query for data.
 async function queryData(dgraphClient, application) {
-  result = [];
-  const { leaveType } = application;
+  result = []
+  const { leaveType } = application
   // Run query.
   const query = `query leaveProcess($leaveProcessType: string) {
     leaveProcess(func: eq(process_name, $leaveProcessType) )  {
@@ -214,6 +226,13 @@ async function queryData(dgraphClient, application) {
                 check
                 next {
                   process_name
+                  processEdges {
+                    process_edge_name
+                    check
+                    next {
+                      process_name
+                    }
+                  }
                 }
               }
             }
@@ -221,91 +240,91 @@ async function queryData(dgraphClient, application) {
         }
       }
     }
-  }`;
-  const vars = { $leaveProcessType: leaveType };
-  const res = await dgraphClient.newTxn().queryWithVars(query, vars);
-  const ppl = res.data;
+  }`
+  const vars = { $leaveProcessType: leaveType }
+  const res = await dgraphClient.newTxn().queryWithVars(query, vars)
+  const ppl = res.data
 
-  return await traversalProcess(ppl.leaveProcess[0], application);
+  return await traversalProcess(ppl.leaveProcess[0], application)
 }
-var result = [];
+var result = []
 
 async function traversalProcess(process, application) {
-  const { processEdges } = process;
-  const processEdgesLength = processEdges ? processEdges.length : -1;
-  console.log(`開始遞迴 & push 當前 ${process.process_name} 節點--------`);
+  const { processEdges } = process
+  const processEdgesLength = processEdges ? processEdges.length : -1
+  console.log(`開始遞迴 & push 當前 ${process.process_name} 節點--------`)
   result.push({
     node_name: process.process_name,
-  });
+  })
   if (processEdgesLength > 0) {
-    console.log(`下一個節點有條件需審核--------`);
+    console.log(`下一個節點有條件需審核--------`)
     for (const [index, edge] of Object.entries(processEdges)) {
       if (await isValidEdge(edge.check, application)) {
-        console.log(`條件通過 & push ${edge.process_edge_name} 節點--------`);
+        console.log(`條件通過 & push ${edge.process_edge_name} 節點--------`)
         result.push({
           node_name: edge.process_edge_name,
-        });
-        console.log(`仍有下一個節點--------`);
+        })
+        console.log(`仍有下一個節點--------`)
         if (edge.next) {
-          console.log('執行子遞迴--------');
-          await traversalProcess(edge.next, application);
+          console.log('執行子遞迴--------')
+          await traversalProcess(edge.next, application)
         } else {
-          break;
+          break
         }
       } else if (index == processEdgesLength - 1) {
-        console.log("條件全部失敗, 回傳結果--------')");
-        break;
+        console.log("條件全部失敗, 回傳結果--------')")
+        break
       } else {
-        console.log(`${edge.process_edge_name} 條件失敗, 看下一個條件--------`);
-        continue;
+        console.log(`${edge.process_edge_name} 條件失敗, 看下一個條件--------`)
+        continue
       }
     }
   } else {
-    console.log("沒有下一個條件節點了, 回傳結果--------')");
+    console.log("沒有下一個條件節點了, 回傳結果--------')")
   }
   // 遍歷結束, 回傳結果
-  return result;
+  return result
 }
 
 function isValidEdge(check, application) {
-  const { username, leaveDays, leaveType } = application;
+  const { username, leaveDays, leaveType } = application
   if (check === 'whatever') {
-    return true;
+    return true
   } else {
-    const replaceArray = ['username', 'leaveDays', 'leaveType'];
-    const replaceWith = [username, leaveDays, leaveType];
+    const replaceArray = ['username', 'leaveDays', 'leaveType']
+    const replaceWith = [username, leaveDays, leaveType]
     for (var i = 0; i <= replaceArray.length - 1; i++) {
       check = check.replace(
         new RegExp('{' + replaceArray[i] + '}', 'gi'),
         typeof replaceWith[i] == 'string'
           ? `'${replaceWith[i]}'`
           : replaceWith[i]
-      );
+      )
     }
-    return eval(check);
+    return eval(check)
   }
 }
 
 async function main() {
-  const dgraphClient = await getDgraphClient();
-  await dropAll(dgraphClient);
-  await setSchema(dgraphClient);
-  await createData(dgraphClient);
+  const dgraphClient = await getDgraphClient()
+  await dropAll(dgraphClient)
+  await setSchema(dgraphClient)
+  await createData(dgraphClient)
 }
 
 function getDgraphClient() {
-  const dgraphClientStub = newClientStub();
-  const dgraphClient = newClient(dgraphClientStub);
+  const dgraphClientStub = newClientStub()
+  const dgraphClient = newClient(dgraphClientStub)
 
-  return dgraphClient;
+  return dgraphClient
 }
 
 main()
   .then(() => {
-    console.log('DONE!');
+    console.log('DONE!')
   })
   .catch((e) => {
-    console.log('ERROR: ', e);
-  });
+    console.log('ERROR: ', e)
+  })
 
-export { getDgraphClient, queryData };
+export { getDgraphClient, queryData }
